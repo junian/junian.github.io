63f0c4689a53db6bbf9a6274badd2f39efd8f23d
Junian Triajianto <author@junian.dev>
2025-11-01 18:20:33 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+const site = 'https://www.junian.dev/';
+const githubAvatar = 'https://github.com/junian.png';
+
 // https://astro.build/config
 export default defineConfig({
+	site,
+	trailingSlash: 'always',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Junian.dev',
+			logo: {
+				src: '/public/favicon-96x96.png',
+				alt: 'Junian.Dev GitHub Avatar',
+				replacesTitle: false,
+			},
+			lastUpdated: true,
+			editLink: {
+				baseUrl: 'https://github.com/junian/junian.github.io/edit/raw/',
+			},
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/junian' },
+				{ icon: 'youtube', label: 'YouTube', href: 'https://youtube.com/@JunianDev' },
+				{ icon: 'seti:firefox', label: 'Tech Blog', href: 'https://www.junian.net/' },
+			],
 			sidebar: [
 				{
 					label: 'Guides',
